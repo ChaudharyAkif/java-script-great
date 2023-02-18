4f4e7ec86d79e651f70c3594270482df8fee7ca8
M AKIF <makifhussainmn@gmail.com>
2023-02-18 22:39:50 +0500

    first coomit 2023

---
+++ app.js
@@ -0,0 +1,53 @@
+document.getElementById("buttonak").onclick = function(){
+alert("akif")
+
+
+}
+document.getElementById("buttonak").onclick = function(){
+    console.log("akif")
+    
+    
+    }
+
+    document.getElementById("buttonak").onclick = function(){
+ 
+        
+        
+        }
+
+
+document.getElementById("ahmed").onclick=function(){
+
+
+}     
+
+
+
+document.getElementById
+
+
+let a = prompt ("what is your name ")
+if ( a= chaudhary akif && a=akif){
+alert( " ok you are ok")
+
+
+}
+
+else{
+alert(" you are not invaild")
+
+}
+
+
+
+
+var students = [ "ahmed","akif" , "112012"]
+students.pop (
+
+)alert("you aare not ")
+
+
+
+
+
+
