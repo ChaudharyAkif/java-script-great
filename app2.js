873d2bd607d4423241d583a0f71451d7bb8ab6f9
M AKIF <makifhussainmn@gmail.com>
2023-02-19 21:48:58 +0500

    han g kya hal h theekio na

---
+++ app2.js
@@ -0,0 +1,16 @@
+let a = prompt (" when is you born");
+if  (a >=  50){
+
+
+ alert  (" what is your "+"" + a ) 
+}
+else   {
+
+alert("you are not correct")
+}
+
+
+// let b = 10;
+// alert (b)
+const b = 20;
+alert (b)
